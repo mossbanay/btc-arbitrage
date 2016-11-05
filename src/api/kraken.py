84f529402a8249491dca8f680ee911eae9eafb10
Moss Banay <mossbanay@gmail.com>
2016-11-05 16:25:20 +1100

    Added ability to get bid and ask prices from Kraken public API

--- src/api/kraken.py
+++ src/api/kraken.py
@@ -1,14 +1,39 @@
-## TODO implement kraken
+import requests, logging
+
+
+class KrakenBadPairNameException(Exception):
+    pass
 
 class KrakenMarket(object):
     def __init__(self):
-        pass
+        self.fee = 0.0026
+        self.api_url = 'https://api.kraken.com/0/public/'
 
     def get_name(self):
         return 'Kraken'
 
-    def get_bid(self):
-        return 0.0
+    def get_ticker(self, pair):
+        req_url = self.api_url + 'Ticker'
 
-    def get_ask(self):
-        return 0.0
+        params = {
+            'pair':pair
+        }
+
+        req = requests.get(url=req_url, params=params)
+
+        data = req.json()
+
+        if data['error']:
+            raise KrakenBadPairNameException('Unable to find pair {} on Kraken'.format(pair))
+
+        return data
+
+    def get_bid(self, pair):
+        ticker = self.get_ticker(pair)
+
+        return ticker['result'][pair]['a'][0]
+
+    def get_ask(self, pair):
+        ticker = self.get_ticker(pair)
+
+        return ticker['result'][pair]['b'][0]
